42b5f54ae48ffac2a3e23ff6f896eefcfaed43da
user.email <dfloresva@unsa.edu.pe>
2020-06-29 18:34:27 -0500

    booleano en views.py

--- appList/travello/views.py
+++ appList/travello/views.py
@@ -7,6 +7,7 @@ def index(request):
 	dest1.desc='The city that never sleeps'
 	dest1.img='destination_1.jpg'
 	dest1.price=700
+	dest1.offer=False
 
 
 	dest2= Destination()
@@ -14,12 +15,14 @@ def index(request):
 	dest2.desc='First Biryani, Then Sherwani'
 	dest2.img='destination_2.jpg'
 	dest2.price=650
+	dest2.offer=True
 
 	dest3= Destination()
 	dest3.name='Begnaluru'
 	dest3.desc='Beatiful city'
 	dest3.img='destination_3.jpg'
 	dest3.price=750
+	dest1.offer=False
 
 	dests=[dest1, dest2, dest3]
 
