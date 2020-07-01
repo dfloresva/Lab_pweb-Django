58b266f0f91aa0524d3f79fca76ce98ad5500cae
user.email <dfloresva@unsa.edu.pe>
2020-07-01 13:39:20 -0500

    Implemento Login

--- appList/accounts/views.py
+++ appList/accounts/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
+def login(request):
+	if request.method== 'POST':
+		username=request.POST['username']
+		password=request.POST['password']
+		
+		user= auth.authentic(username=username,password=password)
+		if user is not None:
+			auth.login(request,user)
+			return redirect("/")
+		else:
+			messages.info(request,'invalid credentials')
+			return redirect("login")
+		
+	else:
+		return render(request, 'login.html')
 def register (request):
 	if(request.method == ' POST'):
 		first_name=request.POST['first_name']
@@ -13,15 +29,19 @@ def register (request):
 
 		if password1==password2:
 			if User.objects.filter(username=username).exists():
-				print('Username taken')
+				messages.info(request,'username Taken')
+				return redirect('register')
 			elif User.objects.filter(email=email).exists():
-				print('email taken')
+				messages.info(request,'email taken')
+				return redirect('register')
 			else:
 				user = User.objects.create_user(username=username,password=password1,email=email,first_name=first_name,last_name=last_name)
 				user.save();
 				print('user created')
+				return redirect('login')
 		else:
-			print('password not matching ..')
+			messages.info(request,'password not matching ..')
+			return redirect('register')
 		return redirect('/')
 	else: 
 		return render(request, 'register.html')
